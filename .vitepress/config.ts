ae5d00185b3435d5fab0aaddf020c1b1a0321bee
Into You <yuxiangll@foxmail.com>
2025-12-04 20:18:19 +0800

    Update config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -137,9 +137,9 @@ export default defineConfig({
       { icon: 'discord', link: discordLink },
     ],
     footer: {
-      message: '用 <span style="color: #e25555;">&#9829;</span> 撰写',
+      message: '用 <span style="color: #cf8181ff;">&#9829;</span> 撰写',
       copyright:
-        '<a class="footer-cc-link" target="_blank" href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a> © 2022-PRESENT Nólëbase 的创作者们',
+        '<a class="footer-cc-link" target="_blank" href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a> © 2022-PRESENT',
     },
     search: {
       provider: 'local',
